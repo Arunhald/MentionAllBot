bc3165c65b7b925ff981bcb55a23b2ae1a90f12a
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2022-03-01 11:06:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
-git clone https://github.com/ImSaravanakrish/MentionAllBot
-cd mentionallbot
-python3 -m bot.py
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/ImSaravanakrish/MentionAllBot.git /MentionAllBot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /main
+fi
+cd /MentionAllBot
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
