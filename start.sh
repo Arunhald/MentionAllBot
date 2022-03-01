b4f485b43942694db5c3b05b7c8bf2ba081bcfa7
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2022-03-01 11:07:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/ImSaravanakrish/MentionAllBot.git /MentionAllBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /main
+  git clone $UPSTREAM_REPO /MentionAllBot
 fi
 cd /MentionAllBot
 pip3 install -U -r requirements.txt
